93fc2c84a0f6a100f6a233d598b1b1a22edc0341
dTap <imtapped@protonmail.com>
2023-02-24 10:02:09 +0700

    Added <Hero />

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
@@ -8,6 +9,7 @@ import Tours from "./components/Tours";
 function App() {
   return <>
     <Navbar />
+    <Hero />
     <About />
     <Services />
     <Tours />
