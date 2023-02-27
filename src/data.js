bb83175594238c57033de21f5f0288f83d36bec3
dTap <imtapped@protonmail.com>
2023-02-27 22:06:26 +0700

    added new links

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const pageLinks = [
 ];
 
 export const socialLinks = [
-  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+  { id: 1, href: 'https://www.facebook.com', icon: 'fab fa-facebook' },
   { id: 2, href: 'https://www.twitter.com', text: 'fab fa-twitter' },
-  { id: 3, href: 'https://www.twitter.com', text: 'fab fa-squarespace' },
+  { id: 3, href: 'https://www.squarespace.com', text: 'fab fa-squarespace' },
 ];
